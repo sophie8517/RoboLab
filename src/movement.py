8c860166bd54f0adc2f151101c67876dfaaf222f
Jonas <jonas.gaffke@mailbox.tu-dresden.de>
2021-09-18 11:01:03 +0200

    Fixed bug

--- src/movement.py
+++ src/movement.py
@@ -24,6 +24,7 @@ class FollowLineResult:
 
 class Movement:
     """Responsible class for moving the robot and the main logic"""
+
     def __init__(self, communication: Communication):
         self.communication = communication
 
@@ -80,7 +81,6 @@ class Movement:
         self.motor_right.position = 0
         self.motor_left.position = 0
         motor_ticks = []
-
 
         bwd = self.sensors.black_white_diff
         speed = 50
@@ -195,10 +195,16 @@ class Movement:
         return absolute_directions
 
     def turn_to_way(self, direction_absolute: Direction) -> None:
+        print(f"{direction_absolute=}")
         """Turns to an absolute direction."""
-        self.position.direction = direction_absolute
 
         direction_relative = Direction((direction_absolute - self.position.direction) % 360)
+
+        self.position.direction = direction_absolute
+
+        print(f"{self.position.direction=}")
+
+        print(f"{direction_relative=}")
 
         angle = int(direction_relative)
 
@@ -269,43 +275,16 @@ class Movement:
         """Calculate next direction. If there is a reachable target return direction to target.
         If no reachable target, return direction to closest undiscovered path.
         Returns None, if everything is discovered."""
-        if self.target is not None:
+
+        if self.target is not None:  # there is a target
             target_path = self.planet.shortest_path_points(self.position.point, self.target)
-            if target_path is None or target_path == []:
+            if target_path is None or target_path == []:  # target not reachable
                 print("Target not reachable")
-                selected_direction = self.smart_discovery.next_direction(self.position.point)
-                if selected_direction is None:
-                    print("Complete, everything discovered")
-                    Sound.play('/home/robot/smb_stage_clear.wav').wait()
-                    response = self.communication.send_exploration_completed("Färdsch!")
-                    if response is not None:
-                        print(f"From mothership: {response}")
-                    else:
-                        print("No response")
-                    return None
-            else:
-                selected_direction = target_path[0].direction
-                print(f"Direction to target: {selected_direction}")
-        else:  # no target
-            selected_direction = self.smart_discovery.next_direction(self.position.point)
-            if selected_direction is None:
-                print("Complete, everything discovered")
-                Sound.play('/home/robot/smb_stage_clear.wav').wait()
-                response = self.communication.send_target_reached("Färdsch!")
-                if response is not None:
-                    print(f"From mothership: {response}")
-                else:
-                    print("No response")
-                return None
+            else:  # target reachable -> return direction
+                return target_path[0].direction
 
-        selected_position = Position(self.position.point, selected_direction)
-        mothership_direction = self.communication.send_path_select(selected_position)
-        if mothership_direction is not None:
-            print(f"Better direction form mothership: {mothership_direction}")
-            selected_direction = mothership_direction
-
-        print(f"Selected direction: {selected_direction}")
-        return selected_direction
+        # no reachable target -> next direction from smart discovery
+        return self.smart_discovery.next_direction(self.position.point)
 
     def got_path_unveiled(self) -> None:
         """Checks if we got path unveiled messages.
@@ -365,9 +344,6 @@ class Movement:
         self.smart_discovery.mark_discovered(path_response.end_position.point,
                                              path_response.end_position.direction)
 
-        if not self.smart_discovery.is_discovered_point(self.position.point):
-            Sound.play('/home/robot/smb_coin.wav').wait()
-
     def main_loop(self) -> bool:
         """Follow line, scan paths, communicate, move to next direction.
         Returns True, if done."""
@@ -390,6 +366,7 @@ class Movement:
         self.got_target()
 
         if not self.smart_discovery.is_discovered_point(self.position.point):
+            Sound.play('/home/robot/smb_coin.wav.wav').wait()
             res_scan_ways_absolute = self.scan_ways()
             try:
                 res_scan_ways_absolute.remove(self.position.direction.turned())
@@ -399,12 +376,26 @@ class Movement:
             self.smart_discovery.add_scan_result(self.position.point, res_scan_ways_absolute)
 
         next_direction = self.get_next_direction()
+        print(f"{next_direction=}")
         if next_direction is None:
+            print("Complete, everything discovered")
+            Sound.play('/home/robot/smb_stage_clear.wav').wait()
+            response = self.communication.send_exploration_completed("Färdsch!")
+            if response is not None:
+                print(f"From mothership: {response}")
+            else:
+                print("No response")
             return True
+
+        mothership_direction = self.communication.send_path_select(Position(self.position.point, next_direction))
+        if mothership_direction is not None:
+            print(f"Better direction form mothership: {mothership_direction}")
+            next_direction = mothership_direction
+
         self.turn_to_way(next_direction)
 
-        time.sleep(3)
-        # Sound.tone(1000, 100).wait()
+        # time.sleep(3)
+        Sound.tone(1000, 100).wait()
 
         self.got_path_unveiled()
         self.got_target()
